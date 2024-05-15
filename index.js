d0feddea08a1342434b3d4d39f20b3e6c6cad02e
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-05-15 22:18:18 +0600

    add route

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:5173","https://moon-dining.web.app/"]
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.avbafwc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
